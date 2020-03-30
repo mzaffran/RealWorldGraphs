5421b15ea45c7456ccacc20fa269d4810567f829
mzaffran <margauxzaffran@gmail.com>
2020-03-30 21:45:14 +0200

    some indentation

--- pagerank.c
+++ pagerank.c
@@ -12,37 +12,38 @@ double* Pagerank(edgelist* g){
 
      unsigned long i;
 
-     for (i=0; i<g->e;i++){
+     for (i=0; i<g->e;i++)
+     {
          d[g->edges[i].s]++ ;
      }
-     for (i=0; i<g->n;i++){
+     for (i=0; i<g->n;i++)
+     {
          pagerank[i] = 1/g->n;
          pageranktemp[i] = 0;
      }
-     for (j=0; j<t ; j++){
+     for (j=0; j<t ; j++)
+     {
          // MatVecctProd (T,P)
-         for(i=0;i< g->e ;i++){
+         for(i=0;i< g->e ;i++)
+         {
              //printf("s t %lu %lu\n",g->edges[i].s,g->edges[i].t);
              pageranktemp[g->edges[i].t]+=pagerank[g->edges[i].s]/d[g->edges[i].s];
          }
 
          somme=0;
-         for(i=0;i< g->n ;i++){
-
+         for(i=0;i< g->n ;i++)
+         {
              pageranktemp[i]=(1-alpha)*pageranktemp[i]+alpha/g->n;
              somme+=pageranktemp[i];
          }
          //normalize
 
-         for(i=0;i< g->n ;i++){
-
+         for(i=0;i< g->n ;i++)
+         {
              pageranktemp[i]+=(1-somme)/g->n;
-
              pagerank[i]=pageranktemp[i]; // update
              pageranktemp[i]=0; //reinitialize
          }
-
-
      }
 
      free(pageranktemp);
@@ -55,8 +56,10 @@ int SearchMax(double* l, unsigned long n){
     int maxindex=0;
     double maxvalue=0;
     unsigned long  i;
-    for (i=0;i<n;i++){
-        if (l[i]>maxvalue)    {
+    for (i=0;i<n;i++)
+    {
+        if (l[i]>maxvalue)
+        {
             maxvalue=l[i];
             maxindex=i;
         }
@@ -69,9 +72,9 @@ void getPages (edgelist *g, int number, char** pagenames){
     unsigned long index;
     int i;
     printf("ranks\n");
-    for (i=0;i<number;i++){
+    for (i=0;i<number;i++)
+    {
         index = SearchMax(pagerank, g->n);
-
         printf("Page with rank %d has ID: %lu ,probability %lf and name %s\n",i+1, index, pagerank[index], pagenames[index]);
         pagerank[index]=0;
     }
@@ -88,24 +91,18 @@ int main(int argc,char** argv){
 	printf("Reading edgelist from file %s\n",argv[1]);
 	g=readedgelist(argv[1]);
 
-    char** pagenames = readpagelist(argv[2], g->n);
-
-
+  char** pagenames = readpagelist(argv[2], g->n);
 
 	printf("Number of nodes: %lu\n",g->n);
 	printf("Number of edges: %lu\n",g->e);
 
-
-    getPages (g, 5 , pagenames);
-
-
+  getPages (g, 5 , pagenames);
 
 	t2=time(NULL);
 
 	printf("- Overall time = %ldh%ldm%lds\n",(t2-t1)/3600,((t2-t1)%3600)/60,((t2-t1)%60));
 
+  free_edgelist(g);
 
-
-    free_edgelist(g);
 	return 0;
 }
